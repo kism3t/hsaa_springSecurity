b39ad2128c17e95f24976a17f2d6f1242b137357
ufuk <altin.ufuk@gmail.com>
2022-10-20 23:05:46 +0200

    Spring Boot Project with Basic Authentication Module

---
+++ src/test/java/de/hsaa/projprog/spring/authentication/springauthentication/SpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsaa.projprog.spring.authentication.springauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
